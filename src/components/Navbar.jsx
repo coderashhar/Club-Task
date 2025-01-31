453c5469b4fc2a93161a34b81cf59abadb76e85d
Mohd. Ashhar Khan <mohd.ashharkhan@Ashhars-Macbook-Air.local>
2025-01-31 13:40:49 +0530

    changed cursor style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,12 +16,18 @@ const Navbar = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">Mozilla Firefox Club</span>
+            <span className="text-xl tracking-tight" style={{ cursor: 'pointer' }}>Mozilla Firefox Club</span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li id="navlink" key={index}>
-                <Link to={item.href} smooth={true} duration={800} offset={item.offset}>{item.label}</Link>
+                <Link 
+                  to={item.href} 
+                  smooth={true} 
+                  duration={800}
+                  offset={item.offset}>
+                    {item.label}
+                </Link>
               </li>
             ))}
           </ul>
